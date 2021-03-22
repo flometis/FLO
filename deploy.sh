a42e6d651fb831dec09a3c66d0629f5edd89a5b7
zorbaproject <tringalinvent@libero.it>
2021-03-22 14:36:20 +0100

    Utilizzo branch di sviluppo di Bran

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,10 @@ for i in $(docker image ls | grep '^<none>.*' | sed 's/[^ ]* *[^ ]* *\([^ ]*\) *
 cp -r /root/flo/backend/files/* /var/lib/docker/volumes/flo_backend/_data/
 git clone https://github.com/zorbaproject/Bran.git /var/lib/docker/volumes/flo_backend/_data/Bran 2> /dev/null | true
 cd /var/lib/docker/volumes/flo_backend/_data/Bran
-#git checkout dev
-#git pull
+git checkout dev
+git pull
 chmod +x /var/lib/docker/volumes/flo_backend/_data/main.py
-
+docker exec flo_backend_1 python3 /var/www/app/Bran/main.py help
 
 # Frontend
 cp -r /root/flo/frontend/files/* /var/lib/docker/volumes/flo_frontend/_data/
