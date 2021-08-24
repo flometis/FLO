4d843db3b70d068d91a7b86e48943c22da03d828
Luca Tringali <luca.tringali@procne.it>
2021-08-24 15:33:31 +0200

    Separazione header e footer

---
+++ frontend/files/hfloader.js
@@ -0,0 +1,20 @@
+var pageName = window.location.pathname.split("/").pop().split(".")[0];
+
+fetch("./header.html")
+  .then(response => {
+      return response.text()
+    })
+  .then(data => {
+      document.querySelector("pageheader").innerHTML = data;
+      document.querySelector(".menu-li-"+pageName).classList.add("active");
+      document.querySelector(".menu-a-"+pageName).classList.add("active");
+      document.querySelector(".menu-a-"+pageName).innerHTML += '<span class="sr-only">current</span>';
+    });
+
+fetch("./footer.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("pagefooter").innerHTML = data;
+  });
