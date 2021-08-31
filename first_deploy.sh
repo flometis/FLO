c8b226cffbf206eb0605f5e5fc69a6a7a5d122da
Luca Tringali <luca.tringali@procne.it>
2021-08-31 18:05:31 +0200

    Fix cronjob pulizia vecchi processi

--- first_deploy.sh
+++ first_deploy.sh
@@ -22,7 +22,7 @@ docker exec flo_backend_1 python3 /var/www/app/Bran/main.py help
 docker cp $appdir/backend/brancfg flo_backend_1:/root/.brancfg
 docker restart flo_backend_1
 docker exec flo_backend_1 service cron start
-docker exec flo_backend_1 cat /etc/cron.d/cleanup-task | crontab -
+docker exec flo_backend_1 /bin/bash -c "cat /etc/cron.d/cleanup-task | crontab -"
 
 # Frontend
 cp -r $appdir/frontend/files/* /var/lib/docker/volumes/flo_frontend/_data/
