bd103ffb18fee67daa836a9ef605f961f877801d
Luca Tringali <luca.tringali@procne.it>
2021-09-01 16:03:21 +0200

    Sample di .env per deploy

--- deploy/files/index.php
+++ deploy/files/index.php
@@ -1,28 +1,97 @@
 <?php
 
+$log = "";
+
 @require("./ip_in_range.php");
+
+//Check IP range
 
 $headers = apache_request_headers();
 
 $ip = "";
 if (@$headers["X-Forwarded-For"]) {
-	    $ips = explode(",",$headers["X-Forwarded-For"]);
-	        $ip  = $ips[0];
+            $ips = explode(",",$headers["X-Forwarded-For"]);
+                $ip  = $ips[0];
 } else {
-	    $ip = $_SERVER['REMOTE_ADDR'];
+            $ip = $_SERVER['REMOTE_ADDR'];
 }
 
-//https://api.github.com/meta
-$range = "172.0.0.0/8";
-if (ipv4_in_range($ip, $range)) {
-	echo "IP ".$ip." in range";
-} else {
-	header('HTTP/1.1 403 Forbidden');
-	echo "<body>\n";
-        echo "<span style=\"color: #ff0000\">IP not allowed to trigger deploy</span>\n"; 
-        echo "</body>\n</html>";
-        exit;
+$ranges = Array();
+array_push($ranges, "5.8.113.166/32", "212.237.53.183/32");
+$opts = [
+  'http' => [
+    'method' => 'GET',
+    'header' => [
+      'User-Agent: PHP'
+    ]
+  ]
+];
+
+$context = stream_context_create($opts);
+$github_range = json_decode(file_get_contents("https://api.github.com/meta", false, $context), true)["hooks"];
+$ranges = array_merge($ranges, $github_range);
+
+$ip_is_in_range = false;
+foreach($ranges as $range) {
+  if (ipv4_in_range($ip, $range)) {
+    $ip_is_in_range = true;
+    break;
+  }
 }
 
-print_r($_POST);
+if (!$ip_is_in_range) {
+  header('HTTP/1.1 403 Forbidden');
+  echo "<body>\n";
+  echo "<span style=\"color: #ff0000\">IP ".$ip." not allowed to trigger deploy.</span>\n";
+  echo "</body>\n</html>";
+  exit;
+  $log .= "IP ".$ip." not allowed to trigger deploy.\n";
+} else {
+  echo "IP ".$ip." in range";
+  $log .= "IP ".$ip." in range.\n";
+}
+
+//Check signature
+
+$secret = getenv('GITHUB_SECRET', true);
+if ($secret !== false && $secret != "") {
+if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
+    exit;
+}
+$githubHeader = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+$rawPost = file_get_contents("php://input");
+$secret = str_replace("sha1=", "", $githubHeader);
+$hash = hash_hmac('sha1', $rawPost, $mySecret);
+if ($hash != $secret){
+  echo "<body>\n";
+  echo "<span style=\"color: #ff0000\">Signature invalid.</span>\n";
+  echo "</body>\n</html>";
+  $log .= "Signature invalid\n";
+  exit;
+}
+} else {
+  $log .= "Github Secret not set.\n";
+  echo "GITHUB_SECRET not set, skipping signature verification";
+}
+
+//Check user
+$github_allowed=getenv('GITHUB_ALLOWED', true);
+if ($github_allowed !== false && $github_allowed != "") {
+print($github_allowed);
+if (!in_array($_POST["pusher"]["name"],explode(",", $github_allowed)) ) {
+  echo "<body>\n";
+  echo "<span style=\"color: #ff0000\">User ".$$_POST["pusher"]["name"]." not allowed to trigger deploy.</span>\n";
+  echo "</body>\n</html>";
+  $log .= "User ".$$_POST["pusher"]["name"]." not allowed to trigger deploy.\n";
+  exit;
+}
+} else {
+  $log .= "User check not enabled.\n";
+}
+
+//Write file
+file_put_contents("deploy.request", strtotime("now"));
+//Write log
+file_put_contents("deploy.log", $log);
 ?>
+
